efb0bb929e6d4030196762028435e83e47ddbc42
Admin <Admin@Admin-PC>
2024-10-01 10:00:43 +0700

    commit 1

--- src/pc_builder/PC_Builder.java
+++ src/pc_builder/PC_Builder.java
@@ -17,5 +17,21 @@ public class PC_Builder {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
+        
+        ThietBi thietBi_1 = new ThietBi("Man hinh A", "Man hinh may tinh", "ASUS", 1300000);
+        ThietBi thietBi_2 = new ThietBi("Ban phim A", "Ban phim may tinh", "ASUS", 400000);
+        ThietBi thietBi_3 = new ThietBi("Chuot A", "Chuot may tinh", "ASUS", 200000);
+        
+        System.out.println(thietBi_1.toString());
+        System.out.println(thietBi_2.toString());
+        System.out.println(thietBi_3.toString());
+        
+        System.out.println("");
+        
+        thietBi_1.themSanPhamTuongThich(thietBi_3);
+        thietBi_1.lietKeCacSanPhamTuongThich(sc);
+        
+        thietBi_3.ten = "Chuot B";
+        thietBi_1.lietKeCacSanPhamTuongThich(sc);
     }
 }
--- src/pc_builder/ThietBi.java
+++ src/pc_builder/ThietBi.java
@@ -11,16 +11,20 @@ import java.util.*;
 public class ThietBi {
     static int id = 1;
     
+    // Du lieu san pham
+    
     String ten;
     String loai;
     String hang;
-    String ma;
+    private String ma;
     long gia;
+
+    private ArrayList<ThietBi> sanPhamTuongThich = new ArrayList<ThietBi>();
     
-    ArrayList<ThietBi> sanPhamTuongThich = new ArrayList<ThietBi>();
+    // Khoi tao san pham moi
     
     private void khoiTaoMaSanPham() {
-        String goi = "SP0000";
+        String goi = "SP000000";
         int l = Integer.toString(id).length();
         ma = goi.substring(0, goi.length()-l-1)+id;
     } 
@@ -30,7 +34,33 @@ public class ThietBi {
         id += 1;
     }
     
-    public void docDuLieu(Scanner sc) {
-        
+    public ThietBi(String ten, String loai, String hang, long gia) {
+        this.ten = ten;
+        this.loai = loai;
+        this.hang = hang;
+        this.gia = gia;
+        khoiTaoMaSanPham ();
+        id += 1;
+    }
+    
+    // Cac ham thao tac voi du lieu
+    
+    public void lietKeCacSanPhamTuongThich(Scanner sc) {
+        for (ThietBi i: sanPhamTuongThich) {
+            System.out.println(i.toString());
+        }
+    }
+    
+    public void themSanPhamTuongThich(ThietBi item) {
+        sanPhamTuongThich.add(item);
+    }
+    
+    public void xoaSanPhamTuongThich(ThietBi item) {
+        sanPhamTuongThich.remove(item);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("ID: %s, %s",ma, ten);
     }
 }
